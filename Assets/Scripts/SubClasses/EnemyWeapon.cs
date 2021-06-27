4d5d395d5fb12e8f4425cc97467692124e4f8198
Aaron Grincewicz <ASGrincewicz@icloud.com>
2021-06-27 16:17:22 -0400

    Sets up enemy weapons.

---
+++ Assets/Scripts/SubClasses/EnemyWeapon.cs
@@ -0,0 +1,15 @@
+﻿// Aaron Grincewicz Veganimus@icloud.com 6/5/2021
+using UnityEngine;
+namespace Veganimus.Platformer
+{
+    public class EnemyWeapon : Weapon
+    {
+        protected override void Shoot()
+        {
+            _canFire = Time.time + _fireRate;
+            var newBullet = Instantiate(_bulletPrefab, _fireOffset.transform.position, _fireOffset.rotation, _poolManager.transform);
+            StartCoroutine(ShootCoolDownRoutine());
+        }
+
+    }
+}
